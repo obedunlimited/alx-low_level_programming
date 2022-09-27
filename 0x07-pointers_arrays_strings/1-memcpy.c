451551344efe7be63eadf15bf7b3e3c809c4dc3f
Obed Favour <93087300+obedunlimited@users.noreply.github.com>
2022-09-27 09:59:08 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,47 +1,18 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * _memcpy - copies n bytes from source memory to destination
  *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
+ * @dest: destination to write to
+ * @src: source to copy from
+ * @n: number of bytes to write
  *
- * Return: Always 0.
+ * Return: pointer to dest
  */
-int main(void)
+char *_memcpy(char *dest, char *src, int n)
 {
-    char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	char *ptr = dest;
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+	while (n--)
+		*ptr++ = *src++;
+
+	return (dest);
 }
