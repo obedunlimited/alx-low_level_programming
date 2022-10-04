c1ca0c2fdf2c8f687fae2d3d181d1b2bfee91bf9
obedunlimited <obedfavour5@gmail.com>
2022-10-04 11:29:07 -0700

    task 1 updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,26 +1,35 @@
 #include "main.h"
-
 /**
-create_array - creates an array of charts, and 
-initialized it with a specific char.
-@size: size of the array
-@c: character  to inster
-Return: NULL if size is zero or if it fails.
-pointer to array if everything is normal.
+ * create_array - array for prints a string
+ * initialized it with a specific char.
+ * @size: size of the array
+ * @c: character  to inster
+ * Return: NULL if size is zero or if it fails.
+ * poinTER
 */
+
 
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int index;
-    
-    if (size == 0)
-            return (NULL);
-    array = malloc(sizeof(char) * size);
-    
-    if (if array == NULL)
-            return (NULL);
-    for (index = 0; index < size;index++)
-            array[index] = c;
-    return (array);
+char *buffer;
+unsigned int position;
+if (size == 0)
+{
+return (NULL);
+}
+/*Define values with malloc*/
+buffer = (char *) malloc(size * sizeof(c));
+if (bufer == 0)
+{
+return (NULL);
+}
+else
+{
+position = 0;
+while (position < size) /*while for array*/
+{
+*(buffer + position) = c;
+position++;
+{
+return (buffer);
 }
